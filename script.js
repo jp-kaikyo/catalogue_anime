707f541eb0db26b65b8a7bc637cedba92cc8dee1
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-12-21 18:51:29 +0100

    V1.7 : html et css pret js a pofiner

--- script.js
+++ script.js
@@ -3,9 +3,20 @@
 ///===============///
 
 
-let button_charger = document.getElementById("charger");
+//  recup des ID  //
+let recherche_anime = document.getElementById("recherche");
+let titre_anime = document.getElementById("titre");
+let visuelle_anime = document.getElementById("visuelle");
+let histoire_anime = document.getElementById("histoire");
+let button_charger_anime = document.getElementById("btn_charger");
+
+
 
 button_charger.onclick = function(){
+    
+    let recherche = recherche_anime.innerHTML;
+
+
     let url = "https://worldtimeapi.org/api/timezone/Europe/Paris";
 
     fetch(url)
@@ -13,8 +24,17 @@ button_charger.onclick = function(){
     .then(data => {
         console.log(data);
         
-        let heure = data.datetime;
+        let titre_anime = data.datetime;
+        let visuelle_anime = data.datetime;
+        let histoire_anime = data.datetime;
         
-        document.getElementById("HEURE").innerHTML = heure;
+        document.getElementById("titre").innerHTML = titre_anime;
+        document.getElementById("visuelle").innerHTML = visuelle_anime;
+        document.getElementById("histoire").innerHTML = histoire_anime;
     })
 }
+
+
+//  débogueur  //
+console.log("[ API ]")
+console.log(recherche)
